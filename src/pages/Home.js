c883caee14dc17e727f6aa5c3923efef36016e3b
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-06-04 01:17:06 -0700

    made portfolio collapsible, it's looking real good

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,17 +18,15 @@ class Home extends Component {
           repos.push(res.data[i]);
         } else if (res.data[i].name === 'character-sheet') {
           repos.push(res.data[i]);
-          console.log(repos);
         }
       }
 
       axios.get('https://api.github.com/repos/E-Bossler/tee-time').then(res => {
         repos.push(res.data);
-        console.log(repos);
         this.setState({ repos });
-        console.log(this.state.repos);
       });
     });
+
     axios.get(`https://api.github.com/users/dustinjack99/events`).then(res => {
       const historyState = this.state.historyState;
 
@@ -68,40 +66,82 @@ class Home extends Component {
   render() {
     const history = this.state.historyState;
     const repos = this.state.repos;
-    const slideshows = document.querySelectorAll('.slide');
-    console.log(repos);
-    slideshows.forEach(initSlideShow);
+    const slideshows1 = document.querySelectorAll('.slide1');
+    const slideshows2 = document.querySelectorAll('.slide2');
+    slideshows1.forEach(initSlideShow1);
+    slideshows2.forEach(initSlideShow2);
 
-    function initSlideShow(slideshow) {
-      const slides = document.querySelectorAll('.slide.tracking-out-contract');
+    function initSlideShow1() {
+      const slides1 = document.querySelectorAll(
+        '.slide1.tracking-out-contract'
+      );
+
       let index = 0,
         time = 5000;
-      slides[index].classList.add('active.tracking-in-expand');
+      slides1[index].classList.add('active1.tracking-in-expand');
 
       setInterval(() => {
-        slides[index].classList.remove('active');
-        slides[index].classList.add('tracking-out-contract');
-        slides[index].classList.remove('tracking-in-expand');
+        slides1[index].classList.remove('active1');
+        slides1[index].classList.add('tracking-out-contract');
+        slides1[index].classList.remove('tracking-in-expand');
 
         index++;
 
-        if (index === slides.length) index = 0;
+        if (index === slides1.length) index = 0;
 
-        slides[index].classList.add('active');
-        slides[index].classList.remove('tracking-out-contract');
-        slides[index].classList.add('tracking-in-expand');
+        slides1[index].classList.add('active1');
+        slides1[index].classList.remove('tracking-out-contract');
+        slides1[index].classList.add('tracking-in-expand');
       }, time);
     }
+
+    function initSlideShow2() {
+      const slides2 = document.querySelectorAll(
+        '.slide2.tracking-out-contract'
+      );
+
+      let index = 0,
+        time = 5000;
+      slides2[index].classList.add('active2.tracking-in-expand');
+
+      setInterval(() => {
+        slides2[index].classList.remove('active2');
+        slides2[index].classList.add('tracking-out-contract');
+        slides2[index].classList.remove('tracking-in-expand');
+
+        index++;
+
+        if (index === slides2.length) index = 0;
+
+        slides2[index].classList.add('active2');
+        slides2[index].classList.remove('tracking-out-contract');
+        slides2[index].classList.add('tracking-in-expand');
+      }, time);
+    }
+
     return (
       <div className='container'>
         <div className='projects'>
           <h2>Current Projects</h2>
+          {repos.map((repo, i) => {
+            return (
+              <div className='slide1 tracking-out-contract' key={i}>
+                <h4>{repo.name}</h4>
+                <ul>
+                  <li>Language: {repo.language}</li>
+                  <li>Open Issues: {repo.open_issues}</li>
+                  <p>{repo.description}</p>
+                  <a href={repo.url}>Link</a>
+                </ul>
+              </div>
+            );
+          })}
         </div>
         <div className='feed'>
           <h2>GitHub Activity</h2>
           {history.map((hist, i) => {
             return (
-              <div className='slide tracking-out-contract' key={i}>
+              <div className='slide2 tracking-out-contract' key={i}>
                 <div className='feedLeft'>
                   <h3>{hist.type}</h3>
                   <p>Event {i + 1}</p>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 
 // Image Imports
 import teetimenat from '../assets/gifs/tee-time-nat-demo.gif';
@@ -7,46 +8,78 @@ import duckpals from '../assets/gifs/duck-demo.gif';
 import scrabble from '../assets/gifs/scrabble-demo.gif';
 
 const Portfolio = () => {
+  function fadeInDuckPals() {
+    let li = document.querySelector('.DuckPals__contentInner');
+    li.classList.add('text-focus-in');
+    li.style.visibility = 'visible';
+  }
+  function fadeInTeeTime() {
+    let li = document.querySelector('.TeeTime__contentInner');
+    li.classList.add('text-focus-in');
+    li.style.visibility = 'visible';
+  }
+  function fadeInScrabble() {
+    let li = document.querySelector('.Scrabble__contentInner');
+    li.classList.add('text-focus-in');
+    li.style.visibility = 'visible';
+  }
+
   return (
     <div className='portfolio'>
       <h2>Portfolio</h2>
-
-      <h4>Projects:</h4>
-
-      <div>
-        <div className='portAnchors'>
-          <a href='https://dustinjack99.github.io/code-quiz/'>Tee Time!</a>
-          <a href='https://github.com/dustinjack99/code-quiz'>Git Repo</a>
-        </div>
-        <div>
-          <div>
-            <h5>Web Client</h5>
+      <div className='portContainer'>
+        <Collapsible
+          trigger='Tee-Time'
+          classParentString='TeeTime'
+          onOpen={fadeInTeeTime}
+          triggerStyle={{ fontSize: '20px', display: 'block' }}
+        >
+          <div className='portAnchors'>
+            <a href='https://dustinjack99.github.io/code-quiz/'>Tee Time!</a>
+            <a href='https://github.com/dustinjack99/code-quiz'>Git Repo</a>
           </div>
-          <img className='portImages' src={teetime} />
-        </div>
-        <div>
-          <h5>Android App made with React Native</h5>
           <div>
-            <img className='portImages' src={teetimenat} />
+            <div>
+              <h5>Web Client</h5>
+            </div>
+            <img className='portImages' src={teetime} />
           </div>
-        </div>
-      </div>
-      <div>
-        <div className='portAnchors'>
-          <a href='https://dustinjack99.github.io/ScrabbleDabble/landing.html'>
-            Scrabble Dabble!
-          </a>
-          <a href='https://github.com/dustinjack99/ScrabbleDabble'>Git Repo</a>
-        </div>
-        <img className='portImagesWide' src={scrabble} />
-      </div>
-      <div>
-        <div className='portAnchors'>
-          <a href='https://duckpals.herokuapp.com'>DuckPals!</a>
-          <a href='https://github.com/austenpturner/DuckPals'>Git Repo</a>{' '}
-        </div>
+          <div>
+            <h5>Android App made with React Native</h5>
+            <div>
+              <img className='portImagesWide' src={teetimenat} />
+            </div>
+          </div>
+        </Collapsible>
+        <Collapsible
+          trigger='Scrabble Dabble'
+          classParentString='Scrabble'
+          onOpen={fadeInScrabble}
+          triggerStyle={{ fontSize: '20px', display: 'block' }}
+        >
+          <div className='portAnchors'>
+            <a href='https://dustinjack99.github.io/ScrabbleDabble/landing.html'>
+              Scrabble Dabble!
+            </a>
+            <a href='https://github.com/dustinjack99/ScrabbleDabble'>
+              Git Repo
+            </a>
+          </div>
+          <img className='portImages' src={scrabble} />
+        </Collapsible>
+        <Collapsible
+          trigger='DuckPals'
+          classParentString='DuckPals'
+          onOpen={fadeInDuckPals}
+          triggerStyle={{ fontSize: '20px', display: 'block' }}
+        >
+          <div className='portAnchors'>
+            <a href='https://duckpals.herokuapp.com'>DuckPals!</a>
+            <a href='https://github.com/austenpturner/DuckPals'>Git Repo</a>
+          </div>
 
-        <img className='portImages' src={duckpals} />
+          <img className='portImagesWide' src={duckpals} />
+        </Collapsible>
       </div>
     </div>
   );

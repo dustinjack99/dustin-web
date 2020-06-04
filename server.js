18bd747dd50a1ee954c769c3d4d0c162580a6092
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-06-04 13:26:24 -0700

    got nodemailer to work! People can send emails directly on my website without opening a mail client. Now to add some validation

--- server.js
+++ server.js
@@ -10,34 +10,35 @@ app.use(express.json());
 
 app.post('/send', (req, res) => {
   console.log(req.body);
+  const { email, comment } = req.body;
 
-  //   let transporter = nodemailer.createTransport({
-  //     host: 'smtp.gmail.com',
-  //     port: 587,
-  //     secure: false,
-  //     auth: {
-  //       user: 'dustin.guy.jackson@gmail.com',
-  //       pass: pass,
-  //     },
-  //   });
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'dustin.guy.jackson@gmail.com',
+      pass: 'lvyijfdzkpctmenv',
+    },
+  });
 
-  //   let info = await transporter.sendMail({
-  //     from: `<${email}>`, // sender address
-  //     to: 'dustin.guy.jackson@gmail.com', // list of receivers
-  //     subject: 'Feedback from dustin-jackson.com', // Subject line
-  //     text: `${comment}`, // plain text body
-  //     html: `<p>${comment}</p>`, // html body
-  //   });
+  transporter.sendMail({
+    from: `<${email}>`, // sender address
+    to: 'dustin.guy.jackson@gmail.com', // list of receivers
+    subject: 'Feedback from dustin-jackson.com', // Subject line
+    text: `${comment}`, // plain text body
+    html: `<p>${comment}</p>`, // html body
+  });
 
-  //   transporter.verify(function (error, success) {
-  //     if (error) {
-  //       console.log(error);
-  //     } else {
-  //       console.log('Server is ready to take our messages');
-  //     }
-  //   });
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take our messages');
+    }
+  });
 
-  //   console.log('Message sent: %s', info.messageId);
+  console.log('Message sent: %s', info.messageId);
 });
 
 app.listen(PORT, function () {

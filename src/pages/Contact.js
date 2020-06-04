b8366340ff25c775d1cbf343d1a24a722ee5a350
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-06-04 01:50:18 -0700

    added styling more in line with portfolio / about to contact. imported nodemailer, setting up form submit functionality

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,26 +1,79 @@
-import React from "react";
+import React, { Component } from 'react';
+import nodemailer from 'nodemailer';
 
-const Contact = () => {
-  return (
-    <>
-      <h2 className="contacthead m-2">Contact</h2>
-      <form>
-        <div>
-          <label for="name">Name</label>
-          <input id="name" type="text" />
-        </div>
-        <div>
-          <label for="email">E-Mail</label>
-          <input id="email" type="email" />
-        </div>
-        <div>
-          <label for="comment">Comments / Inquiries</label>
-          <textarea rows="5" id="comment"></textarea>
-        </div>
-        <input id="submit" type="submit" />
-      </form>
-    </>
-  );
-};
+async function main(email, comment) {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+  });
+
+  let info = await transporter.sendMail({
+    from: `<${email}>`, // sender address
+    to: 'dustin.guy.jackson@gmail.com', // list of receivers
+    subject: 'Feedback from dustin-jackson.com', // Subject line
+    text: `${comment}`, // plain text body
+    html: `<p>${comment}</p>`, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+}
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      comment: '',
+    };
+  }
+
+  submitForm() {
+    console.log('wow');
+
+    const email = this.state.email;
+    const comment = this.state.comment;
+    main(email, comment);
+  }
+
+  changeEmail(e) {
+    const email = e.target.value;
+    this.setState({ email });
+    console.log(this.state.email);
+  }
+
+  changeComment(e) {
+    const comment = e.target.value;
+    this.setState({ comment });
+    console.log(this.state.comment);
+  }
+
+  render() {
+    return (
+      <>
+        <h2 className='contacthead m-2'>Contact</h2>
+        <form>
+          <div>
+            <label htmlFor='email'>E-Mail</label>
+            <input
+              onChange={value => this.changeEmail(value)}
+              id='email'
+              type='email'
+            />
+          </div>
+          <div>
+            <label htmlFor='comment'>Comments / Inquiries</label>
+            <textarea
+              onChange={value => this.changeComment(value)}
+              rows='5'
+              id='comment'
+            ></textarea>
+          </div>
+          <input id='submit' type='submit' onClick={this.submitForm} />
+        </form>
+      </>
+    );
+  }
+}
 
 export default Contact;

27a77b44c713225c14dd28cba8c01c2cebcc09c0
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-06-09 12:22:24 -0700

    changes to production server axios requests for nodemailer

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,37 +4,42 @@ import swal from 'sweetalert2';
 
 function sendNodemailer(email, comment, emailErr) {
   console.log(email, comment, emailErr);
-  axios.post('http://localhost:8080/email', { email }).then(res => {
-    if (res.data === 'noEmail') {
-      emailErr.style.display = 'block';
-    } else if (res.data === 'valid') {
-      emailErr.style.display = 'none';
-      swal
-        .fire({
-          title: 'Are you sure?',
-          text: 'Send an email to me?',
-          icon: 'warning',
-          showCancelButton: true,
-        })
-        .then(results => {
-          if (results.value) {
-            swal.fire(
-              'Email sent!',
-              "Thanks for dropping a line, I'll respond back ASAP",
-              'success'
-            );
+  axios
+    .post('https://pacific-wave-49973.herokuapp.com/email', { email })
+    .then(res => {
+      if (res.data === 'noEmail') {
+        emailErr.style.display = 'block';
+      } else if (res.data === 'valid') {
+        emailErr.style.display = 'none';
+        swal
+          .fire({
+            title: 'Are you sure?',
+            text: 'Send an email to me?',
+            icon: 'warning',
+            showCancelButton: true,
+          })
+          .then(results => {
+            if (results.value) {
+              swal.fire(
+                'Email sent!',
+                "Thanks for dropping a line, I'll respond back ASAP",
+                'success'
+              );
 
-            axios.post('http://localhost:8080/send', { email, comment });
-          } else if (results.dismiss === swal.DismissReason.cancel) {
-            swal.fire(
-              'Email not sent!',
-              "Don't be shy, I'll still be here :)",
-              'info'
-            );
-          }
-        });
-    }
-  });
+              axios.post('https://pacific-wave-49973.herokuapp.com/send', {
+                email,
+                comment,
+              });
+            } else if (results.dismiss === swal.DismissReason.cancel) {
+              swal.fire(
+                'Email not sent!',
+                "Don't be shy, I'll still be here :)",
+                'info'
+              );
+            }
+          });
+      }
+    });
 }
 class Contact extends Component {
   constructor(props) {

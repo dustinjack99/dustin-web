e10da806e1ca2b93e56cacb7a16b63b00fa65895
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-10-12 11:27:55 -0700

    fixed the pdf files not loading

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,109 +1,109 @@
-import React from 'react';
-import profile from '../assets/images/placeholder.jpg';
-import Collapsible from 'react-collapsible';
+import React from "react";
+import profile from "../assets/images/placeholder.jpg";
+import Collapsible from "react-collapsible";
 
 // Image / Asset Imports
-import angular from '../assets/images/angular.png';
-import azure from '../assets/images/azure.png';
-import cmd from '../assets/images/cmd.png';
-import css from '../assets/images/css.png';
-import docker from '../assets/images/docker.png';
-import git from '../assets/images/git.png';
-import html from '../assets/images/html.png';
-import jslogo from '../assets/images/jslogo.png';
-import ts from '../assets/images/tslogo.png';
-import mongo from '../assets/images/mongo.png';
-import mysql from '../assets/images/mysql.png';
-import node from '../assets/images/node.png';
-import postgres from '../assets/images/postgres.png';
-import react from '../assets/images/react.png';
-import resume from '../assets/resumes/Developer Resume (16).pdf';
-import sass from '../assets/images/sass.png';
+import angular from "../assets/images/angular.png";
+import azure from "../assets/images/azure.png";
+import cmd from "../assets/images/cmd.png";
+import css from "../assets/images/css.png";
+import docker from "../assets/images/docker.png";
+import git from "../assets/images/git.png";
+import html from "../assets/images/html.png";
+import jslogo from "../assets/images/jslogo.png";
+import ts from "../assets/images/tslogo.png";
+import mongo from "../assets/images/mongo.png";
+import mysql from "../assets/images/mysql.png";
+import node from "../assets/images/node.png";
+import postgres from "../assets/images/postgres.png";
+import react from "../assets/images/react.png";
+import resume from "../assets/resumes/Developer Resume (16).pdf";
+import sass from "../assets/images/sass.png";
 
 const About = () => {
   function fadeInProgLang() {
-    let li = document.querySelector('.ProgLang__contentInner');
-    li.classList.add('text-focus-in');
-    li.style.visibility = 'visible';
+    let li = document.querySelector(".ProgLang__contentInner");
+    li.classList.add("text-focus-in");
+    li.style.visibility = "visible";
   }
   function fadeInWebLang() {
-    let li = document.querySelector('.WebLang__contentInner');
-    li.classList.add('text-focus-in');
-    li.style.visibility = 'visible';
+    let li = document.querySelector(".WebLang__contentInner");
+    li.classList.add("text-focus-in");
+    li.style.visibility = "visible";
   }
   function fadeInTools() {
-    let li = document.querySelector('.Tools__contentInner');
-    li.classList.add('text-focus-in');
-    li.style.visibility = 'visible';
+    let li = document.querySelector(".Tools__contentInner");
+    li.classList.add("text-focus-in");
+    li.style.visibility = "visible";
   }
   function fadeInSkills() {
-    let li = document.querySelector('.Skills__contentInner');
-    li.classList.add('text-focus-in');
-    li.style.visibility = 'visible';
+    let li = document.querySelector(".Skills__contentInner");
+    li.classList.add("text-focus-in");
+    li.style.visibility = "visible";
   }
 
   function picMajesty() {
-    let pic = document.querySelector('#profile-pic');
-    let li = document.querySelector('.Collapsible__contentInner');
-    li.classList.add('pulsate-bck');
-    pic.classList.add('glow');
+    let pic = document.querySelector("#profile-pic");
+    let li = document.querySelector(".Collapsible__contentInner");
+    li.classList.add("pulsate-bck");
+    pic.classList.add("glow");
   }
 
   function beautyWarn() {
     let beauty =
-      document.querySelector('.Collapsible__trigger.is-closed') ||
-      document.querySelector('.Collapsible__trigger.is-open');
+      document.querySelector(".Collapsible__trigger.is-closed") ||
+      document.querySelector(".Collapsible__trigger.is-open");
 
-    if (beauty.textContent !== 'so majestic...') {
-      beauty.textContent = 'click i dare you';
+    if (beauty.textContent !== "so majestic...") {
+      beauty.textContent = "click i dare you";
     }
   }
 
   function warnAway() {
     let beauty =
-      document.querySelector('.Collapsible__trigger.is-closed') ||
-      document.querySelector('.Collapsible__trigger.is-open');
+      document.querySelector(".Collapsible__trigger.is-closed") ||
+      document.querySelector(".Collapsible__trigger.is-open");
 
-    if (beauty.textContent !== 'so majestic...') {
-      beauty.textContent = '***click here for Beauty***';
+    if (beauty.textContent !== "so majestic...") {
+      beauty.textContent = "***click here for Beauty***";
     }
   }
 
   function clickClear() {
     let beauty =
-      document.querySelector('.Collapsible__trigger.is-closed') ||
-      document.querySelector('.Collapsible__trigger.is-open');
-    if (beauty.textContent !== 'so majestic...') {
-      beauty.textContent = 'so majestic...';
+      document.querySelector(".Collapsible__trigger.is-closed") ||
+      document.querySelector(".Collapsible__trigger.is-open");
+    if (beauty.textContent !== "so majestic...") {
+      beauty.textContent = "so majestic...";
     } else {
-      beauty.textContent = '***click here for Beauty***';
+      beauty.textContent = "***click here for Beauty***";
     }
   }
 
   return (
     <>
-      <div className='about-me'>
-        <div className='left-about'>
+      <div className="about-me">
+        <div className="left-about">
           <div
-            className='picDiv'
+            className="picDiv"
             onMouseEnter={beautyWarn}
             onMouseOut={warnAway}
             onClick={clickClear}
           >
             <Collapsible
-              trigger='***click here for Beauty***'
+              trigger="***click here for Beauty***"
               triggerStyle={{
-                color: 'whitesmoke',
-                display: 'block',
-                cursor: 'pointer',
-                paddingBottom: '20px',
+                color: "whitesmoke",
+                display: "block",
+                cursor: "pointer",
+                paddingBottom: "20px",
               }}
               onOpen={picMajesty}
             >
-              <img alt='dustinjackson' id='profile-pic' src={profile} />
+              <img alt="dustinjackson" id="profile-pic" src={profile} />
             </Collapsible>
           </div>
-          <div id='bio'>
+          <div id="bio">
             <p>
               Dustin is a developer familiar with agile development, leading
               creative teams, and has a deep love for learning new technologies
@@ -115,8 +115,8 @@ const About = () => {
               He also loves hiking with his daughter, annoying his cats, and
               playing way too much Dwarf Fortress.
             </p>
-            <div className='profLinks'>
-              <a target='_blank' rel='noopener noreferrer' href={resume}>
+            <div className="profLinks">
+              <a target="_blank" rel="noopener noreferrer" href={resume}>
                 Check out my Resume!
               </a>
             </div>
@@ -124,127 +124,127 @@ const About = () => {
         </div>
         <p></p>
 
-        <div className='skillbox'>
-          <h5 className='hover'>Click Titles!</h5>
-          <div className='skills'>
+        <div className="skillbox">
+          <h5 className="hover">Click Titles!</h5>
+          <div className="skills">
             <Collapsible
-              classParentString='ProgLang'
+              classParentString="ProgLang"
               onOpen={fadeInProgLang}
               triggerStyle={{
-                fontSize: '20px',
-                display: 'block',
-                color: '#4d89f8',
-                cursor: 'pointer',
+                fontSize: "20px",
+                display: "block",
+                color: "#4d89f8",
+                cursor: "pointer",
               }}
-              trigger='Languages:'
+              trigger="Languages:"
             >
               <ul>
                 <li>
-                  Javascript{' '}
-                  <img alt='javascript' className='wider' src={jslogo} />
+                  Javascript{" "}
+                  <img alt="javascript" className="wider" src={jslogo} />
                 </li>
                 <li>
-                  Typescript <img alt='typescript' src={ts} />
+                  Typescript <img alt="typescript" src={ts} />
                 </li>
                 <li>
-                  Node.js <img alt='nodejs' className='wider' src={node} />
+                  Node.js <img alt="nodejs" className="wider" src={node} />
                 </li>
                 <li>
-                  Git <img alt='git' src={git} />
+                  Git <img alt="git" src={git} />
                 </li>
                 <li>
-                  Shell scripting{' '}
-                  <img alt='shellscript' className='wider' src={cmd} />
+                  Shell scripting{" "}
+                  <img alt="shellscript" className="wider" src={cmd} />
                 </li>
               </ul>
             </Collapsible>
             <Collapsible
-              classParentString='WebLang'
+              classParentString="WebLang"
               onOpen={fadeInWebLang}
               triggerStyle={{
-                fontSize: '20px',
-                display: 'block',
-                color: '#4d89f8',
-                cursor: 'pointer',
+                fontSize: "20px",
+                display: "block",
+                color: "#4d89f8",
+                cursor: "pointer",
               }}
-              trigger='Web:'
+              trigger="Web:"
             >
               <ul>
                 <li>
-                  HTML5 <img alt='html' src={html} />
+                  HTML5 <img alt="html" src={html} />
                 </li>
                 <li>
                   CSS
-                  <img alt='css' src={css} />
+                  <img alt="css" src={css} />
                 </li>
                 <li>
                   SASS
-                  <img alt='sass' src={sass} />
+                  <img alt="sass" src={sass} />
                 </li>
                 <li>
                   Docker
-                  <img alt='docker' src={docker} />
+                  <img alt="docker" src={docker} />
                 </li>
                 <li>
                   Azure
-                  <img alt='azure' src={azure} />
+                  <img alt="azure" src={azure} />
                 </li>
               </ul>
             </Collapsible>
             <Collapsible
-              classParentString='Tools'
+              classParentString="Tools"
               onOpen={fadeInTools}
               triggerStyle={{
-                fontSize: '20px',
-                display: 'block',
-                color: '#4d89f8',
-                cursor: 'pointer',
+                fontSize: "20px",
+                display: "block",
+                color: "#4d89f8",
+                cursor: "pointer",
               }}
-              trigger='Tools:'
+              trigger="Tools:"
             >
               <ul>
                 <li>
-                  Angular <img alt='angular' src={angular} />
+                  Angular <img alt="angular" src={angular} />
                 </li>
                 <li>
-                  React / React Native{' '}
-                  <img alt='react' className='wider' src={react} />
+                  React / React Native{" "}
+                  <img alt="react" className="wider" src={react} />
                 </li>
                 <li>
-                  MySQL <img alt='mysql' className='wider' src={mysql} />
+                  MySQL <img alt="mysql" className="wider" src={mysql} />
                 </li>
                 <li>
-                  Postgres{' '}
-                  <img alt='postgres' className='wider' src={postgres} />
+                  Postgres{" "}
+                  <img alt="postgres" className="wider" src={postgres} />
                 </li>
                 <li>
-                  NoSQL <img alt='mongodb' src={mongo} />
+                  NoSQL <img alt="mongodb" src={mongo} />
                 </li>
               </ul>
             </Collapsible>
             <Collapsible
-              classParentString='Skills'
+              classParentString="Skills"
               onOpen={fadeInSkills}
               triggerStyle={{
-                fontSize: '20px',
-                display: 'block',
-                color: '#4d89f8',
-                cursor: 'pointer',
+                fontSize: "20px",
+                display: "block",
+                color: "#4d89f8",
+                cursor: "pointer",
               }}
-              trigger='Skills:'
+              trigger="Skills:"
             >
-              <ul className='textSkills'>
-                <li className='skill'>
+              <ul className="textSkills">
+                <li className="skill">
                   Lead large, highly collaborative teams.
                 </li>
-                <li className='skill'>
+                <li className="skill">
                   Professionally trained public speaker.
                 </li>
-                <li className='skill'>
+                <li className="skill">
                   Working in demanding, fast-paced work environment.
                 </li>
-                <li className='skill'>Agile software development</li>
-                <li className='skill'>Test Driven Development</li>
+                <li className="skill">Agile software development</li>
+                <li className="skill">Test Driven Development</li>
               </ul>
             </Collapsible>
           </div>
